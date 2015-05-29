a6b91499d4a05b706c7484927a687e037ff3b96e
David Jensen <david.lgj@gmail.com>
2015-05-29 09:47:00 +0200

    Directive to add integer and float parsing

---
+++ src/type-parser.js
@@ -0,0 +1,47 @@
+/**
+ * It might be a bug, but currently input[type=number] does not add
+ * a parser, so the model gets populated with a string. It does however stop non numbers so it
+ * must have some preproccessing. Anyway, this adds parser before schema-validate hooks into it.
+ * FIXME: this is still not a complete solution. Inputting a string in an input[type=number] results
+ * in parsers never firing and ngModel value removed. So no validation from schema-validate either.
+ */
+angular.module('schemaForm').directive('sfTypeParser', function() {
+  return {
+    restrict: 'A',
+    scope: false,
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      var once = scope.$watch(attrs.sfTypeParser, function(schema) {
+        if (!schema) {
+          return;
+        }
+
+        var isNumber  = schema.type.indexOf('number') !== -1;
+        var isInteger = schema.type.indexOf('integer') !== -1;
+        var numberRE  = /^[0-9]*$/;
+        // Use index of since type can be either an array with two values or a string.
+        if (isNumber || isInteger) {
+          // The timing here seems to work. i.e. we get in before schema-validate
+          ngModel.$parsers.push(function(viewValue) {
+            var value;
+            if (isNumber) {
+              value = parseFloat(viewValue);
+            } else if (numberRE.test(viewValue)) {
+              // We test the value to check that it's a valid integer, otherwise we can easily
+              // get float -> integer parsing behind the scenes.
+              value = parseInt(viewValue, 10);
+            }
+            console.log('parser', numberRE.test(viewValue), viewValue, value)
+            if (value === undefined || isNaN(value)) {
+              //Let the validation fail. @FIXME: it fails with "required" for some reason.
+              return viewValue;
+            }
+            return value;
+          });
+        }
+
+        once();
+      });
+    }
+  };
+});
